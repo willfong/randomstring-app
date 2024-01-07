32abc5c73378d086c7b51e61c950dc77917d3635
Will Fong <willfong@hey.com>
2024-01-07 14:08:09 +0700

    lint and adding url

--- index.mjs
+++ index.mjs
@@ -8,28 +8,29 @@ const app = express();
 app.use(morgan("combined"));
 
 const generate = (l) => {
-	const a = cryptoRandomString({ length: l, type: "alphanumeric" });
-	const b = cryptoRandomString({ length: l, type: "distinguishable" });
-	const c = cryptoRandomString({ length: l, type: "ascii-printable" });
-	return `Random Stuff:\n${a}\n\nEasy to read:\n${b}\n\nPasswords:\n${c}\n\n`;
+  const a = cryptoRandomString({ length: l, type: "alphanumeric" });
+  const b = cryptoRandomString({ length: l, type: "distinguishable" });
+  const c = cryptoRandomString({ length: l, type: "ascii-printable" });
+  const d = cryptoRandomString({ length: l, type: "url-safe" });
+  return `Random Stuff:\n${a}\n\nEasy to read:\n${b}\n\nPasswords:\n${c}\n\nURL-safe:\n${d}\n\n`;
 };
 
 app.get("/:len(\\d{0,})", async (req, res) => {
-	const len = parseInt(req.params.len, 10) || 32;
-	res.send(generate(len > 128 ? 128 : len));
+  const len = parseInt(req.params.len, 10) || 32;
+  res.send(generate(len > 128 ? 128 : len));
 });
 
 app.get("/alb-health-check", (req, res) => {
-	res.send("ok");
+  res.send("ok");
 });
 
 app.use((req, res) => {
-	res.status(404).send("404 - File not found");
+  res.status(404).send("404 - File not found");
 });
 
 app.use((err, req, res, next) => {
-	console.log(err);
-	res.status(500).send("500 - Internal server error");
+  console.log(err);
+  res.status(500).send("500 - Internal server error");
 });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
